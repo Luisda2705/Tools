4818d8627651ad253c3372a05b55d13478b2814f
Luis <43306068+Luisda2705@users.noreply.github.com>
2023-09-21 12:44:24 -0600

    Update Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -1,4 +1,4 @@
-apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm
+apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap
 
 su linux
 brew install naabu dog
