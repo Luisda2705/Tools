d232cf60ca7238f05a1ef1264733025c28745efe
root <root@Linux>
2023-09-22 21:13:10 +0000

    update

--- BTools.sh
+++ BTools.sh
@@ -1,1 +1,6 @@
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install naabu dog gtop htop bottom gau
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/linux/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
