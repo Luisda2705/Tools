73513b3cead5ff322d06b53981921cae452683b0
Luis <43306068+Luisda2705@users.noreply.github.com>
2023-09-22 15:14:21 -0600

    Update BTools.sh

--- BTools.sh
+++ BTools.sh
@@ -1,6 +1,5 @@
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install naabu dog gtop htop bottom gau
-
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/linux/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install naabu dog gtop htop bottom gau
