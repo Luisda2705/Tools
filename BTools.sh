d39f6908847819e99802f2b739fa21b45ecf9729
Luis <lusartc27@gmail.com>
2023-09-22 00:35:35 +0000

    update

---
+++ BTools.sh
@@ -0,0 +1,1 @@
+brew install naabu dog gtop htop bottom gau
--- Tools.sh
+++ Tools.sh
@@ -1,4 +1,1 @@
-apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap
-
-su linux
-brew install naabu dog gtop htop bottom
+apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap ffuf
