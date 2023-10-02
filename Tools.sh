ef59e05bc94590b74db24630eb7472ad64cf8e48
root <root@Linux>
2023-10-02 21:03:46 +0000

    update

--- Tools.sh
+++ Tools.sh
@@ -1,1 +1,1 @@
-apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap ffuf glances
+apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap ffuf glances duf
