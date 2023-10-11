04888614bce08ffdee2972bc8451513172c89316
Luisda2705 <luisda27055@gmail.com>
2023-10-11 20:48:28 +0000

    update

--- Tools.sh
+++ Tools.sh
@@ -1,1 +1,4 @@
 apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap ffuf glances duf nano
+
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+mv $HOME/.nano/* /usr/share/nano/
