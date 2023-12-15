0174a29190f3a009a6de5d1709a9ad215a58d8bb
Luisda2705 <luisda27055@gmail.com>
2023-12-15 21:30:54 +0000

    15:30:54 - 15/12/23

--- Tools.sh
+++ Tools.sh
@@ -1,5 +1,4 @@
 apt-get install -y net-tools exa tree ripgrep fd-find neofetch git npm nmap ffuf glances duf nano bat gh procs
-
 apt-get install build-essential
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
