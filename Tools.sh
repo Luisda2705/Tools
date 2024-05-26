121371c2d63f5d27655eed0a3145a4c5c398ab9d
Luisda2705 <lusartc27@gmail.com>
2024-05-26 19:40:38 +0000

    13:40:38 - 26/05/24

--- Tools.sh
+++ Tools.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-APPS="net-tools eza tree ripgrep fd-find neofetch git npm nmap ffuf glances duf nano bat gh fzf"
+APPS="net-tools eza tree ripgrep fd-find neofetch git npm nmap ffuf glances duf nano bat gh fzf curl"
 
 apt-get install -y $APPS
 apt-get install -y build-essential
