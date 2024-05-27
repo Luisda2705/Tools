880cec0b47a088eb1d74bfbf0e0780b24a078865
Luisda2705 <lusartc27@gmail.com>
2024-05-27 05:11:30 +0000

    23:11:30 - 26/05/24

--- Tools.sh
+++ Tools.sh
@@ -5,6 +5,10 @@ APPS="net-tools eza tree ripgrep fd-find neofetch git npm nmap ffuf glances duf 
 apt-get install -y $APPS
 apt-get install -y build-essential
 
+cd $HOME
+cd Setup/
+/bin/bash Setup.sh
+
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 mv $HOME/.nano/* /usr/share/nano/
