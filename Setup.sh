86e713d3f192fa54b405d5456442d3adb7bc2ee5
Luisda2705 <luisda27055@gmail.com>
2025-11-14 13:41:58 -0600

    13:41:58 - 14/11/25

---
+++ Setup.sh
@@ -0,0 +1,2 @@
+git config --global user.name "Luisda2705"
+git config --global user.email "luisda27055@gmail.com"
--- Tools.sh
+++ Tools.sh
@@ -6,16 +6,19 @@ apt-get install -y build-essential
 cd $HOME/Setup/
 sh Setup.sh
 
-# Install NanoRC
+# Install Nanorc
 cp $HOME/Tools/nanorc /etc/nanorc
+
+cd $HOME
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
 # MacOS Folder
 # mv $HOME/.nano/* /opt/homebrew/Cellar/nano/*/share/nano/
 
+# Linux Folder
 mv $HOME/.nano/* /usr/share/nano/
-rm $HOME/.nano/
 
+rm $HOME/.nano/
 rm $HOME/.nanorc
 
 clear
