a5c545a3142dd185a6c2a798eeb8a3f1c08c2856
Luisda2705 <luisda27055@gmail.com>
2025-12-01 14:16:05 -0600

    14:16:05 - 01/12/25

--- Tools.sh
+++ Tools.sh
@@ -1,8 +1,8 @@
+
 APPS="net-tools eza tree ripgrep fd-find neofetch git npm nmap ffuf glances duf nano bat gh fzf curl unzip wget httpx cmatrix"
 
 apt-get install -y $APPS
 apt-get install -y build-essential
-
 
 cd $HOME/Setup/
 sh Setup.sh
